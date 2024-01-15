ed372dfc1bd9e11fc25adbeeef67e92317c840f4
HieuNguyen <nguyenhieu11032002@gmail.com>
2024-01-15 15:04:11 +0700

    validation create-user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
-    email: string;
-    password: string;
-    name: string;
+  @IsEmail({}, { message: 'Invalid email' })
+  @IsNotEmpty({
+    message: 'Email is required',
+  })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty({
+    message: 'Password is required',
+  })
+  password: string;
+
+  @IsString()
+  name: string;
 }
